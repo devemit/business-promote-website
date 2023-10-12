3a86d7aeee149ba4250fa19ac9f6cb7e58ccdb8b
Mitko <mitkoiliev505@gmail.comm>
2023-10-12 13:11:47 +0200

    location map size fix

--- components/location.tsx
+++ components/location.tsx
@@ -4,7 +4,7 @@ import map from '../public/macedonia-presentation-map.jpg';
 export default function Location() {
    return (
       <div className='h-screen w-full'>
-         <div className='relative w-[100%] h-full'>
+         <div className='relative w-[100%] h-[500px] md:h-full'>
             <Image src={map} alt='skopje' fill />
          </div>
       </div>
