e454e1ae4ebd0380703ade27892ea551a6efe8cd
Mitko <mitkoiliev505@gmail.comm>
2023-10-12 10:51:49 +0200

    types file fix

--- types.ts
+++ types.ts
@@ -2,3 +2,8 @@ export interface Props {
     toggleNav: boolean;
     handleToggle: () => void;
   }
+  export interface CardProps {
+    title:string;
+    description:string;
+    image:string;
+  }
